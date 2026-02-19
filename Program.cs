ccd328f0daee0b5d2d5551f5dfeb3ecedba1eb7f
Jgarc <Jgarc@DESKTOP-KRTL31P>
2026-02-18 21:09:20 -0800

    This took me a liitle longer because i did it backwards at first then moved things around wasnt getting the name or age to apper all i had to do was capitalise the N in name and A in age.

--- Program.cs
+++ Program.cs
@@ -1,25 +1,33 @@
-﻿namespace COMP003A.LectureActivity6
+﻿
+namespace COMP003A.LectureActivity6
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+  using System;
+ public class student
         {
-
-        }
-        public class student
-        {
-
             public string Name;
             public int Age;
- public student (string name, int age)
-        {
-            name = name;
-            age = age;
-        }
+            public student(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
             public void DisplayInfo()
             {
                 Console.WriteLine($"Name: {Name}, Age: {Age}");
             }
+        }
+         internal class program
+        {
+            static void Main(string[] args)
+            {
+                student student1 = new student("Joseph", 23);
+                student student2 = new student("Braxton", 30);
+                student1.DisplayInfo();
+                student2.DisplayInfo();
+                Console.WriteLine("\nAfter modifying student1 age run DisplayInfo again:\n");
+                student1.Age = 40;
+                student1.DisplayInfo();
+                student2.DisplayInfo();
         }
     }
 }
