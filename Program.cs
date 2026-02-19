d555fe93d62083283136be5cdaa5a3c9e52327bd
Jgarc <Jgarc@DESKTOP-KRTL31P>
2026-02-18 17:39:20 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace COMP003A.LectureActivity6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public class student
+        {
+
+            public string Name;
+            public int Age;
+ public student (string name, int age)
+        {
+            name = name;
+            age = age;
+        }
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Name: {Name}, Age: {Age}");
+            }
+        }
+    }
+}
